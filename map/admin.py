1af42be175e0ad4aebb4faaecd459c34aae64d7a
Eric Hippler <hippler.eric@gmail.com>
2021-03-02 09:39:33 -0500

    Admin display setup

--- map/admin.py
+++ map/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import Route, Update, Segment
 
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+class UpdateAdmin(admin.ModelAdmin):
+    list_display = ["id", "timestamp", "next_update"]
+
+class SegmentAdmin(admin.ModelAdmin):
+    list_display = ["route", "last_update", "post_range"]
+
 # Register your models here.
 
-admin.site.register(Route)
-admin.site.register(Update)
-admin.site.register(Segment)
+admin.site.register(Route, RouteAdmin)
+admin.site.register(Update, UpdateAdmin)
+admin.site.register(Segment, SegmentAdmin)
