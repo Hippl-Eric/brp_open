68ef353bf80aeec1b990b9a14917f20bf293e383
Eric Hippler <hippler.eric@gmail.com>
2021-03-02 09:43:23 -0500

    Style and display update information, add click function to polyline

--- map/static/map/site.js
+++ map/static/map/site.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-    request_token(load_map);
+    requestToken(loadMap);
 });
 
-function request_token(callback) {
+let myMap = null
+let highlightPolyline = null
+
+function requestToken(callback) {
     // Get accessToken from backend
     // Temp solution to prevent committing token to source control
     // Still visible to site user when public, TODO: setup restrictions
@@ -26,12 +29,11 @@ function request_token(callback) {
     .then(data => callback(data.key));
 }
 
-function load_map(token) {
+function loadMap(token) {
     // Leaflet.js map
-    var myMap = L.map('leaflet');
+    myMap = L.map('map');
     const defBounds = L.latLngBounds(L.latLng(38.0306, -78.85724), L.latLng(35.29641, -83.29456))
     myMap.fitBounds(defBounds);
-
     let tile = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
@@ -42,10 +44,12 @@ function load_map(token) {
     })
     tile.addTo(myMap);
 
-    get_route_data(myMap)
+    // Request route data
+    getRouteData(myMap)
 };
 
-function get_route_data(map) {
+function getRouteData(map) {
+    // Request route data for open and closures
     const csrftoken = Cookies.get('csrftoken');
     let request = new Request(
         "/route_data",
@@ -60,36 +64,43 @@ function get_route_data(map) {
     fetch(request)
     .then(response => response.json())
     .then(data => {
-        load_route(map, data);
+        loadRoute(map, data);
+        loadUpdate(data);
     });
 };
 
-function load_route(map, data) {
+function loadUpdate(data) {
+    // Format and apply time and date to DOM
+    const strUpdate = formatUpdate(data['update'])
+    const strNextUpdate = formatNextUpdate(data['next_update'])
+    document.getElementById('update').innerHTML = `Road Status as of <b>${strUpdate}</b>`
+    document.getElementById('next-update').innerHTML = `Next update: <b>${strNextUpdate}</b>`
+}
 
-    // Parse data
-    const update = data['update'];
-    const nextUpdate = data['next_update'];
-    const segments = data['segments'] // Array
-
+function loadRoute(map, data) {
     // Create polylines and add to map
-    let polylines = createPolylines(segments);
+    let polylines = createPolylines(data['segments']);
     polylines.forEach(polyline => {
         polyline.addTo(map);
     });
 };
 
-function createPolylines(segment_array) {
+function createPolylines(segmentArray) {
     // Parse segment data and return array of L.polyline objects
-
     let polylines = [];
-    segment_array.forEach(segObject => {
-        let color = 'red';
-        if (segObject['status'] === 'Open') {
-            color = 'green';
+    segmentArray.forEach(segObject => {
+        let color = 'green';
+        if (segObject['status'] == 'Closed') {
+            color = 'red';
         };
+
+        // Create polyline
         polyline = L.polyline(segObject['points'], {color: color, weight: 10, opacity: 0.5});
 
-        // Create hover tooltip
+        // Polyline click function
+        polyline.on('click', zoomPoly)
+
+        // Polyline hover tooltip
         text = `<b>Mileposts:</b> ${segObject['post_range']}<br>
         <b>Crossroads:</b> ${segObject['cross_roads']}<br>
         <b>Status:</b> ${segObject['status']}<br>
@@ -99,4 +110,32 @@ function createPolylines(segment_array) {
         polylines.push(polyline);
     });
     return polylines
+}
+
+function zoomPoly() {
+    myMap.fitBounds(this.getBounds());
+    // highlightPolyline = L.polyline(this.getLatLngs(), {color: 'blue', weight: 20, opacity: 0.5});
+    // highlightPolyline.addTo(myMap);
+}
+
+function formatUpdate(isoString) {
+    // Format: Tuesday, March 2nd at 5:00 AM
+    const date = new Date(isoString);
+    const options = { weekday: 'long', month: 'long', day: 'numeric' };
+    let strDate = date.toLocaleDateString('en-NY', options);
+
+    // Format time to 12 Hour (https://stackoverflow.com/a/17538193/14984232)
+    let strTime = date.toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3")
+
+    return `${strDate} at ${strTime}`;
+}
+
+function formatNextUpdate(isoString) {
+    // Format: March 3rd
+    // TODO off by one day due to timezone conversion 
+    const date = new Date(isoString);
+    const options = {month: 'long', day: 'numeric' };
+    let strDate = date.toLocaleDateString('en-NY', options);
+                
+    return `${strDate}`;
 }
